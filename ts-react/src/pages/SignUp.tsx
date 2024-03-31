fb7d50fe1d5b4168882edd30edd8e44b5b96c009
Daniel Salinas Merino <danielsalinas305@gmail.com>
2024-03-31 15:13:55 +0200

    feature: First set of changes on SignUp

--- ts-react/src/pages/SignUp.tsx
+++ ts-react/src/pages/SignUp.tsx
@@ -8,42 +8,52 @@ import { FormField } from "../components/FormField";
 
 export const SignUp = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [email, setEmail] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [password, setPassword] = useState<string>("");
 
   useEffect(() => {
     setErrorMessage(null);
   }, [email, password]);
 
+  const handleChangeEmail = (value: string) => {
+    setEmail(value);
+  };
+
+  const handleChangePassword = (value: string) => {
+    setPassword(value);
+  };
+
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    fetch("https://backend-login-placeholder.deno.dev/api/users", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.status === "error") {
+          throw new Error(data.code);
+        }
+      })
+      .then(() => {
+        navigate("/success");
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
   return (
     <main className="signup-container">
-      <form
-        className="signup-form"
-        onSubmit={(event) => {
-          event.preventDefault();
-
-          fetch("https://backend-login-placeholder.deno.dev/api/users", {
-            method: "POST",
-            body: JSON.stringify({ email, password }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.status === "error") {
-                throw new Error(data.code);
-              }
-            })
-            .then(() => {
-              navigate("/success");
-            })
-            .catch((error) => {
-              setErrorMessage(error.message);
-            });
-        }}
-      >
+      <form className="signup-form" onSubmit={handleOnSubmit}>
         <Title>Sign up with email</Title>
 
         <p>Enter your email address to create an account.</p>
@@ -52,7 +62,7 @@ export const SignUp = () => {
           id="email"
           labelText="Your email"
           value={email}
-          onChange={setEmail}
+          onChange={handleChangeEmail}
           type={"email"}
         />
 
@@ -60,7 +70,7 @@ export const SignUp = () => {
           id="password"
           labelText="Your password"
           value={password}
-          onChange={setPassword}
+          onChange={handleChangePassword}
           type={"password"}
         />
 
